4fbe9b3c40ab249836f40d63db9759c5816fbbdc
yahayakenny <y.hkehinde@yahoo.com>
2020-07-05 23:42:11 +0100

    bug: Fixed a few bugs

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-const first = document.getElementById("user")
+const first = document.getElementById("first")
 const myImg = document.getElementById("myImg")
-const last = document.getElementById("last")
+const text = document.getElementById("text")
 const mail = document.getElementById("mail")
 const city = document.getElementById("city")
 const state = document.getElementById("state")
 const country = document.getElementById("country")
 const title = document.getElementById("title")
 
+
 const getUser = async() => {
-    const name = await fetch("https://randomuser.me/api")
-    const data = await name.json()
+    const userInfo = await fetch("https://randomuser.me/api")
+    const data = await userInfo.json()
 
     const image = data.results[0].picture.large;
 
     myImg.src = image;
 
-    title.innerHTML = data.results[0].name.title;
+    title.innerHTML = `Title: ${data.results[0].name.title}`;
 
-    first.innerHTML = data.results[0].name.first;
+    first.innerHTML = `First-Name : ${data.results[0].name.first}`;
 
     const last_name = data.results[0].name.last
 
     const lastName = document.createElement('p');
 
-    const textnode = document.createTextNode(last_name)
+    const textNode = document.createTextNode(last_name)
 
-    const x = lastName.appendChild(textnode)
 
-    console.log(x)
-
-    last.appendChild(x)
-
-    mail.innerHTML = data.results[0].email
+    let surName = lastName.appendChild(textNode)
     
-    city.innerHTML = data.results[0].location.city
 
-    state.innerHTML = data.results[0].location.state
+    text.appendChild(surName)
+
+    mail.innerHTML = `Email Address : ${data.results[0].email}`
     
-    country.innerHTML = data.results[0].location.country
+    city.innerHTML = `City: ${data.results[0].location.city}`
+
+    state.innerHTML = `State : ${data.results[0].location.state}`
+    
+    country.innerHTML = `Country: ${data.results[0].location.country}`
     
     // console.log(data)
 //    .then(res => res.json())
